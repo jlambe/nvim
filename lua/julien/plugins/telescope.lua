ea668dedc2692b85facece0c4a35492527fc48c2
Julien <julien@themosis.com>
2024-01-02 09:26:37 +0100

    Format telescope.lua file.

--- lua/julien/plugins/telescope.lua
+++ lua/julien/plugins/telescope.lua
@@ -16,33 +16,43 @@ return {
                 file_browser = {
                     hijack_netrw = false
                 },
+                ['ui-select'] = {
+                    require('telescope.themes').get_dropdown()
+                },
             }
         },
         config = function()
-            require('telescope').load_extension('fzf')
-            require('telescope').load_extension('file_browser')
+            local telescope = require('telescope')
+
+            telescope.load_extension('fzf')
+            telescope.load_extension('file_browser')
+            telescope.load_extension('ui-select')
         end,
         keys = {
-            { '<leader>sf', function() require('telescope.builtin').find_files() end, desc = 'Open telescope files fuzzy finder.' },
-            { '<leader>sb', function() require('telescope.builtin').buffers() end, desc = 'Open telescope buffers fuzzy finder.' },
+            { '<leader>sf', function() require('telescope.builtin').find_files() end,                                                        desc = 'Open telescope files fuzzy finder.' },
+            { '<leader>sb', function() require('telescope.builtin').buffers() end,                                                           desc = 'Open telescope buffers fuzzy finder.' },
             { '<leader>sg', function() require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > '), no_ignore = true }) end, desc = 'Open telescope grep search.' },
-            { '<leader>so', function() require('telescope.builtin').find_files({ no_ignore = true }) end, desc = 'Open telescope files fuzzy finder including all igored files.' },
-            { '<leader>fb', function()
-                local telescope = require('telescope')
+            { '<leader>so', function() require('telescope.builtin').find_files({ no_ignore = true }) end,                                    desc = 'Open telescope files fuzzy finder including all igored files.' },
+            {
+                '<leader>fb',
+                function()
+                    local telescope = require('telescope')
 
-                local function telescope_buffer_dir()
-                    return vim.fn.expand('%:p:h')
-                end
+                    local function telescope_buffer_dir()
+                        return vim.fn.expand('%:p:h')
+                    end
 
-                telescope.extensions.file_browser.file_browser({
-                    path = '%:p:h',
-                    cwd = telescope_buffer_dir(),
-                    respect_gitignore = false,
-                    hidden = true,
-                    grouped = true,
-                    previewer = false,
-                })
-            end, desc = 'Open telescope file browser.' }
+                    telescope.extensions.file_browser.file_browser({
+                        path = '%:p:h',
+                        cwd = telescope_buffer_dir(),
+                        respect_gitignore = false,
+                        hidden = true,
+                        grouped = true,
+                        previewer = false,
+                    })
+                end,
+                desc = 'Open telescope file browser.'
+            }
         }
     },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
@@ -53,5 +63,7 @@ return {
             'nvim-lua/plenary.nvim'
         }
     },
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+    },
 }
-

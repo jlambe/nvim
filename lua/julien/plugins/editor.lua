48b45be90c5f675bd6cf3f207468146952467ef6
Julien <julien@themosis.com>
2024-05-22 13:20:14 +0200

    Add vim-tmux-navigator plugin.

--- lua/julien/plugins/editor.lua
+++ lua/julien/plugins/editor.lua
@@ -48,6 +48,16 @@ return {
         },
     },
     {
+        'christoomey/vim-tmux-navigator',
+        keys = {
+            { '<C-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+            { '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+            { '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+            { '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+            { '<C-^>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+        },
+    },
+    {
         'danymat/neogen',
         opts = {
             snippet_engine = 'luasnip',

acbb7214667312665bc9e25847b08f4ecc151097
Julien <julien@themosis.com>
2024-05-22 13:25:30 +0200

    Remove unused previous tmux pane remap.

--- lua/julien/plugins/editor.lua
+++ lua/julien/plugins/editor.lua
@@ -54,7 +54,6 @@ return {
             { '<C-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
             { '<C-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
             { '<C-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
-            { '<C-^>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
         },
     },
     {

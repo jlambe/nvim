050ec668ad4f41814ffe297aa5676c6434502784
Julien <julien@themosis.com>
2024-09-26 15:42:13 +0200

    Add binding to copy current file path to clipboard.

--- lua/julien/remap.lua
+++ lua/julien/remap.lua
@@ -1,27 +1,39 @@
+local set = vim.keymap.set
+
 -- Remap leader key
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
 
 -- Open Netrw
--- vim.keymap.set('n', '<C-p>', vim.cmd.Ex)
+-- vim.keymap.set("n", "<C-p>", vim.cmd.Ex)
 
 -- Navigate
-vim.keymap.set('n', '<C-d>', '<C-d>zz');
-vim.keymap.set('n', '<C-u>', '<C-u>zz');
-vim.keymap.set('n', 'n', 'nzzzv');
-vim.keymap.set('n', 'N', 'Nzzzv');
+set("n", "<C-d>", "<C-d>zz");
+set("n", "<C-u>", "<C-u>zz");
+set("n", "n", "nzzzv");
+set("n", "N", "Nzzzv");
 
 -- Move lines in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+set("v", "J", ":m '>+1<CR>gv=gv")
+set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- Quickfix
-vim.keymap.set('n', '<leader>cn', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<leader>cp', '<cmd>cprev<CR>zz')
-vim.keymap.set('n', '<leader>cc', '<cmd>cclose<CR>')
+set("n", "<leader>cn", "<cmd>cnext<CR>zz")
+set("n", "<leader>cp", "<cmd>cprev<CR>zz")
+set("n", "<leader>cc", "<cmd>cclose<CR>")
+
+-- Copy current file path to clipboard
+set("n", "<leader>fp", function ()
+    local filepath = vim.fn.expand("%")
+    vim.fn.setreg("+", filepath)
+end)
 
 -- Terminal
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>')
+set("t", "<ESC>", "<C-\\><C-n>")
 
 -- Tmux Sessionizer
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
+set("n", "<C-f>", "<cmd>silent !tmux display-popup -E tmux-sessionizer<CR>")
+
+-- Lua Reload
+set("n", "<leader>x", "<cmd>source %<CR>", { desc = "Execute the current file" })
+
